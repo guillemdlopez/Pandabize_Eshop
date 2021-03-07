38a50b2b7838a55c3b5ab793b44dc5873e34cdc0
Guillem Delas <guillemdelas@hotmail.com>
2021-03-07 18:17:02 +0100

    permitted sign up field: first_name, last_name and photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
     skip_before_action :verify_authenticity_token
     
     include Pundit
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
     after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
     private
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo])
+    end
     
     def skip_pundit?
         devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
